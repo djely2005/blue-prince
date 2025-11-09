a615604272fe47ea47db17b4de084be31e4fe0c8
lyna-lab <boussafeurlyna04@gmail.com>
2025-11-09 21:10:24 +0100

    base-1

---
+++ src/entities/rooms.py
@@ -0,0 +1,150 @@
+# On importe les "plans" (vos classes)
+from src.entities.blue_room import BlueRoom
+from src.entities.yellow_room import YellowRoom
+from src.entities.violet_room import VioletRoom
+from src.entities.orange_room import OrangeRoom
+from src.entities.green_room import GreenRoom
+from src.entities.red_room import RedRoom
+from src.entities.door import Door
+from src.utils.direction import Direction
+from src.utils.lock_state import LockState
+from src.utils.rarity import Rarity
+
+entrance_hall = BlueRoom(
+    name="Entrance Hall", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.TOP),Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.RIGHT), Door(LockState.UNLOCKED, Direction.LEFT)],
+    rarity=Rarity.COMMON
+)
+
+ante_chambre = BlueRoom(
+    name="Antechamber", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM)],
+    rarity=Rarity.COMMON
+)
+
+parlor = BlueRoom(
+    name="Parlor", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)],
+    rarity=Rarity.COMMON
+)
+
+closet = BlueRoom(
+    name="Closet", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM)],
+    rarity=Rarity.COMMON
+)
+
+nook = BlueRoom(
+    name="Nook",
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)], 
+    rarity=Rarity.COMMON
+)
+
+den = BlueRoom(
+    name="Den",
+    price=1, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.RIGHT), Door(LockState.UNLOCKED, Direction.LEFT)], 
+    rarity=Rarity.COMMON
+)
+
+pantry = BlueRoom(
+    name="Pantry",
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)], 
+    rarity=Rarity.COMMON
+)
+
+commissary = YellowRoom(
+    name="Commissary",
+    price=1, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)], 
+    rarity=Rarity.STANDARD
+)
+
+laundry_room = YellowRoom(
+    name="LaundryRoom",
+    price=1,
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM)],
+    rarity=Rarity.RARE
+)
+
+bedroom = VioletRoom(
+    name="BedRoom", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)],
+    rarity=Rarity.COMMON
+)
+
+boudoir = VioletRoom(
+    name="Boudoir", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT)],
+    rarity=Rarity.STANDARD
+)
+
+hallway = OrangeRoom(
+    name="Hallway", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.LEFT), Door(LockState.UNLOCKED, Direction.RIGHT)],
+    rarity=Rarity.COMMON
+)
+
+passageway = OrangeRoom(
+    name="Passageway", 
+    price=2, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.TOP), Door(LockState.UNLOCKED, Direction.LEFT), Door(LockState.UNLOCKED, Direction.RIGHT)],
+    rarity=Rarity.COMMON
+)
+
+terrace = GreenRoom(
+    name="Terrace", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM)],
+    rarity=Rarity.COMMON
+)
+
+veranda = GreenRoom(
+    name="Veranda", 
+    price=2, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.TOP)],
+    rarity=Rarity.UNUSUAL
+)
+
+lavatory = RedRoom(
+    name="Lavatory", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM)],
+    rarity=Rarity.COMMON
+)
+
+gymnasium = RedRoom(
+    name="Gymnasium", 
+    price=0, 
+    doors=[Door(LockState.UNLOCKED, Direction.BOTTOM), Door(LockState.UNLOCKED, Direction.RIGHT), Door(LockState.UNLOCKED, Direction.LEFT)],
+    rarity=Rarity.STANDARD
+)
+
+# Maybe I can use a deck for room picker not sure if I will keep it
+
+FULL_ROOM_DECK = [
+    parlor,
+    closet,
+    nook,
+    den,
+    pantry,
+    commissary,
+    laundry_room,
+    bedroom,
+    boudoir,
+    hallway,
+    passageway,
+    terrace,
+    veranda,
+    lavatory,
+    gymnasium
+]
--- src/utils/rarity.py
+++ src/utils/rarity.py
@@ -2,7 +2,8 @@ from enum import Enum
 
 class Rarity(Enum):
     COMMON = 0
-    UNUSUAL = 1
-    RARE = 2
+    STANDARD = 1
+    UNUSUAL = 2
+    RARE = 3
 
 # I will compare it to a random number (luck of the player)
